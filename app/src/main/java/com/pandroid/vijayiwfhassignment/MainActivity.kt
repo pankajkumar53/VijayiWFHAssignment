50623a0f13d30b18b49ec01f485ee057ad4f77ff
Pankaj Kumar <148642166+pankajkumar53@users.noreply.github.com>
2025-10-22 14:08:46 +0530

    Initial commit

---
+++ app/src/main/java/com/pandroid/vijayiwfhassignment/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.pandroid.vijayiwfhassignment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pandroid.vijayiwfhassignment.ui.theme.VijayiWFHAssignmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VijayiWFHAssignmentTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    VijayiWFHAssignmentTheme {
+        Greeting("Android")
+    }
+}
